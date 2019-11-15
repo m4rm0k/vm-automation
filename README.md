61b5e012c859a2bb1448374e86155d25a56683b8
Pernat1y <1ns@pm.me>
2019-11-15 15:24:39 +0200

    v0.2
    Added multi-threading
    Many other updates

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # vm-automation
-Python script that can be used to test software/scripts/etc. on VMs (currently only VirtualBox is supported).
+Python script that can be used to test software/scripts/etc on VMs (currently only VirtualBox is supported).
 
 Both Windows and Linux are supported as host OS.
 
@@ -9,75 +9,23 @@ Both Windows and Linux are supported as host OS.
 
 Note. If snapshot does not exist on specific VM it will be skipped.
 
-* vm_gui - start VM with (1) or without (0) GUI
-* vboxmanage_path - path to 'vboxmanage' executable. On Windows you can leave it as-is and add VirtualBox'es folder in $PATH. Example: ['vboxmanage']
+* vm_gui - start VM with GUI (vm_gui=1) or without GUI (vm_gui=0)
+* vboxmanage_path - path to 'vboxmanage' executable. On Windows you can leave it as-is and add VirtualBox'es folder in $PATH. Example: 'vboxmanage'
 * vm_guest_username - login for guest OS. Example: 'user'
 * vm_guest_password - password for guest OS. Example: 'P@ssw0rd'
 * remote_folder - directory, where executable is uploaded (on guest host). Example: 'C:\\\\Users\\\\user\\\\Desktop\\\\'
-* vm_delay - Execution timeout in seconds. Example: 30
-* vm_start_timeout - VM start timeout in seconds. Example: 20
-* vboxmanage_timeout - global timeout for all vboxmanage commands. Example: 60
+* timeout - global timeout for all vboxmanage commands (seconds). Example: 60
 
 # Usage:
-vm-automation.py binary.exe
+python vm-automation.py binary.exe
 
 # TODO:
-* Run multiple VMs in parallel
+* Control how many threads run simultaneously (currently equals to number of VMs)
 * Optionally disable network for guest
 * Optional commands to run before/after analysis
 
 # Example video:
-* Version 0.1 on Windows host:
+* Windows host (version 0.2):
 
-<a href="http://www.youtube.com/watch?feature=player_embedded&v=IMrZaJPqVlA" target="_blank"><img src="http://img.youtube.com/vi/IMrZaJPqVlA/0.jpg" width="240" height="180" border="10" /></a>
-
-# Example output:
-```
-PS C:\tmp> python .\vm-automation.py .\putty.exe
-2019-11-11 17:48:37,942 [INFO] Using file .\putty.exe
-2019-11-11 17:48:37,943 [INFO] VMs: ['w10_1903_x64', 'w10_1903_x86']; Snapshots: ['live']
-2019-11-11 17:48:37,985 [INFO] VirtualBox version: 6.0.14r133895
-
-2019-11-11 17:48:37,985 [INFO] Using VM w10_1903_x64 with snapshot live
-2019-11-11 17:48:37,986 [INFO] Stopping VM w10_1903_x64
-2019-11-11 17:48:38,036 [INFO] VM not running
-2019-11-11 17:48:38,036 [INFO] Restoring VM w10_1903_x64 to snapshot live
-2019-11-11 17:48:38,121 [INFO] Snapshot restored
-2019-11-11 17:48:38,121 [INFO] Starting VM w10_1903_x64
-2019-11-11 17:48:50,439 [INFO] VM started
-2019-11-11 17:48:50,439 [INFO] Uploading file .\putty.exe as C:\Users\user\Desktop\635887651.exe to VM w10_1903_x64
-2019-11-11 17:48:52,500 [INFO] File uploaded
-2019-11-11 17:48:52,501 [INFO] Taking screenshot on VM w10_1903_x64 as w10_1903_x64_live_1.png
-2019-11-11 17:48:52,620 [INFO] Executing file C:\Users\user\Desktop\635887651.exe on VM w10_1903_x64
-2019-11-11 17:48:57,321 [INFO] File executed successfully
-2019-11-11 17:49:02,322 [INFO] Taking screenshot on VM w10_1903_x64 as w10_1903_x64_live_2.png
-2019-11-11 17:49:02,409 [INFO] Waiting for results. Timeout: 30 seconds.
-2019-11-11 17:49:32,410 [INFO] Taking screenshot on VM w10_1903_x64 as w10_1903_x64_live_3.png
-2019-11-11 17:49:32,504 [INFO] Stopping VM w10_1903_x64
-2019-11-11 17:49:33,200 [INFO] VM stopped
-2019-11-11 17:49:33,200 [INFO] Restoring VM w10_1903_x64 to snapshot live
-2019-11-11 17:49:33,280 [INFO] Snapshot restored
-2019-11-11 17:49:33,281 [INFO] Done
-2019-11-11 17:49:33,282 [INFO] Using VM w10_1903_x86 with snapshot live
-2019-11-11 17:49:33,282 [INFO] Stopping VM w10_1903_x86
-2019-11-11 17:49:33,360 [INFO] VM not running
-2019-11-11 17:49:33,360 [INFO] Restoring VM w10_1903_x86 to snapshot live
-2019-11-11 17:49:33,456 [INFO] Snapshot restored
-2019-11-11 17:49:33,456 [INFO] Starting VM w10_1903_x86
-2019-11-11 17:49:45,791 [INFO] VM started
-2019-11-11 17:49:45,791 [INFO] Uploading file .\putty.exe as C:\Users\user\Desktop\670555300.exe to VM w10_1903_x86
-2019-11-11 17:49:47,292 [INFO] File uploaded
-2019-11-11 17:49:47,292 [INFO] Taking screenshot on VM w10_1903_x86 as w10_1903_x86_live_1.png
-2019-11-11 17:49:47,435 [INFO] Executing file C:\Users\user\Desktop\670555300.exe on VM w10_1903_x86
-2019-11-11 17:49:51,855 [INFO] File executed successfully
-2019-11-11 17:49:56,856 [INFO] Taking screenshot on VM w10_1903_x86 as w10_1903_x86_live_2.png
-2019-11-11 17:49:56,946 [INFO] Waiting for results. Timeout: 30 seconds.
-2019-11-11 17:50:26,946 [INFO] Taking screenshot on VM w10_1903_x86 as w10_1903_x86_live_3.png
-2019-11-11 17:50:27,058 [INFO] Stopping VM w10_1903_x86
-2019-11-11 17:50:27,705 [INFO] VM stopped
-2019-11-11 17:50:27,706 [INFO] Restoring VM w10_1903_x86 to snapshot live
-2019-11-11 17:50:27,796 [INFO] Snapshot restored
-2019-11-11 17:50:27,796 [INFO] Done
-PS C:\tmp>
-```
+<a href="http://www.youtube.com/watch?feature=player_embedded&v=nIj4cW_miuA" target="_blank"><img src="http://img.youtube.com/vi/nIj4cW_miuA/0.jpg" width="320" height="240" border="10" /></a>
 
--- vm-automation.py
+++ vm-automation.py
@@ -2,185 +2,185 @@ import logging
 import os
 import random
 import re
+import string
 import subprocess
 import sys
+import threading
 import time
 
 # Options
 vms_list = ['w10_1903_x64', 'w10_1903_x86']
 snapshots_list = ['live']
-# threads = len(vms_list)  # Can be set to static (e.g 'threads = 2'). Not used yet.
+threads = len(vms_list)  # Ignored
 vm_gui = 1  # 1 (gui) or 0 (headless)
-vboxmanage_path = ['vboxmanage']
+vboxmanage_path = 'vboxmanage'
 vm_guest_username = 'user'
 vm_guest_password = 'P@ssw0rd'
 remote_folder = 'C:\\Users\\user\\Desktop\\'
-vm_delay = 30
-vm_start_timeout = 20
-vboxmanage_timeout = 60
+timeout = 60
 
 # Logging options
 logger = logging.getLogger('vm-automation')
 logging.basicConfig(format='%(asctime)s [%(levelname)s] %(message)s', level=logging.INFO)
 
-
 # Check for command line args and file
 args = ''.join(sys.argv[1:])
 if args in ['-h', '--help', '/?'] or not args:
-    logger.info('Usage: {} path_to_file'.format(sys.argv[0]))
+    print(f'Usage: {args[0]} path_to_file')
     exit()
 elif os.path.isfile(args):
-    logger.info('Using file {}'.format(args))
     local_file = args
     local_file_extension = re.findall('\\.\\w+$', local_file)
 else:
-    logger.error('File "{}" does not exist. Exiting.'.format(args))
+    print(f'File "{args}" does not exist. Exiting.')
     exit()
 
 
-# Wrapper for vboxmanage command
-def vboxmanage(args):
-    try:
-        cmd = subprocess.run(vboxmanage_path + args, capture_output=True, timeout=vboxmanage_timeout, text=True)
-        return cmd.returncode, cmd.stdout, cmd.stderr
-    except FileNotFoundError:
-        logging.error('Unable to start vboxmanage. Exiting.')
-        exit()
-
-
-# VirtualBox version
-def vm_version():
-    version = vboxmanage(['--version'])
-    if version[0] == 0:
-        logging.info('VirtualBox version: {}'.format(version[1]))
-    else:
-        logging.error('Unknown derp. Code: {}. Exiting.'.format(version[0]))
-        exit()
-
-
 # Start virtual machine
-def vm_start(vm_name):
+def vm_start(vm_name, snapshot_name):
     if vm_gui == 1:
         vm_start_type = 'gui'
     elif vm_gui == 0:
         vm_start_type = 'headless'
     else:
         vm_start_type = 'gui'
-        logging.error('hide_window option should be 1 (headless) or 0 (gui). Using 0 (gui).')
-    logging.info('Starting VM {}'.format(vm_name))
-    a, b, c = vboxmanage(['startvm', vm_name, '--type', vm_start_type])
-    if a == 0:
-        logging.info('VM started')
-    elif a == 1:
-        logging.error('VM already running')
-        exit()
+    logging.info(f'{vm_name}({snapshot_name}): Starting VM')
+    cmd = subprocess.run([vboxmanage_path, 'startvm', vm_name, '--type', vm_start_type], capture_output=True,
+                         timeout=timeout, text=True)
+    if cmd.returncode == 0:
+        logging.info(f'{vm_name}({snapshot_name}): VM started')
     else:
-        logging.error('Unknown error: ' + str(a))
+        logging.error(f'{vm_name}({snapshot_name}): Error while starting VM. Code: {cmd.returncode}')
         exit()
 
 
 # Stop virtual machine
-def vm_stop(vm_name):
-    logging.info('Stopping VM {}'.format(vm_name))
-    a, b, c = vboxmanage(['controlvm', vm_name, 'poweroff'])
-    if a == 0:
-        logging.info('VM stopped')
-    elif a == 1:
-        logging.info('VM not running')
+def vm_stop(vm_name, snapshot_name):
+    logging.info(f'{vm_name}({snapshot_name}): Stopping VM')
+    cmd = subprocess.run([vboxmanage_path, 'controlvm', vm_name, 'poweroff'], capture_output=True, timeout=timeout,
+                         text=True)
+    if cmd.returncode == 0:
+        logging.debug(f'{vm_name}({snapshot_name}): VM stopped')
+        time.sleep(3)
+    elif cmd.returncode == 1:
+        logging.debug(f'{vm_name}({snapshot_name}): VM not running')
     else:
-        logging.error('Unknown error: ' + str(a))
+        logging.error(f'{vm_name}({snapshot_name}): Unknown error: {cmd.returncode}')
 
 
 # Restore snapshot for virtual machine
 def vm_restore(vm_name, snapshot_name):
-    logging.info('Restoring VM {} to snapshot {}'.format(vm_name, snapshot_name))
-    a, b, c = vboxmanage(['snapshot', vm_name, 'restore', snapshot_name])
-    if a == 0:
-        logging.info('Snapshot restored')
-    elif a == 1:
-        logging.error('Snapshot does not exist')
+    logging.info(f'{vm_name}({snapshot_name}): Restoring snapshot')
+    cmd = subprocess.run([vboxmanage_path, 'snapshot', vm_name, 'restore', snapshot_name], capture_output=True,
+                         timeout=timeout, text=True)
+    if cmd.returncode == 0:
+        logging.debug(f'{vm_name}({snapshot_name}): Snapshot restored')
+        time.sleep(3)
     else:
-        logging.error('Unknown error: ' + str(a))
+        logging.error(f'{vm_name}({snapshot_name}): Error while restoring snapshot. Code: {cmd.returncode}')
 
 
 # Execute file on VM
-def vm_start_on_vm(vm_name, vm_login, vm_guest_password, remote_file):
-    logging.info('Executing file {} on VM {}'.format(remote_file, vm_name))
+def vm_start_on_vm(vm_name, snapshot_name, vm_guest_username, vm_guest_password, remote_file):
+    logging.info(f'{vm_name}({snapshot_name}): Executing file {remote_file}')
     _ = 0
-    while _ < vm_start_timeout:
-        a, b, c = vboxmanage(
-            ['guestcontrol', vm_name, '--username', vm_guest_username, '--password', vm_guest_password, 'start',
-             remote_file])
-        if a == 0:
-            logging.info('File executed successfully')
+    while _ < timeout:
+        cmd = subprocess.run([vboxmanage_path,
+                              'guestcontrol', vm_name, '--username', vm_guest_username, '--password',
+                              vm_guest_password, 'start',
+                              remote_file], capture_output=True, timeout=timeout, text=True)
+        if cmd.returncode == 0:
+            logging.debug(f'{vm_name}({snapshot_name}): File executed successfully')
             break
         else:
-            # File not executed. Waiting for VM to start.
+            # Waiting for VM to start
             time.sleep(1)
             _ += 1
-        if _ == vm_start_timeout:
-            logging.error('Timeout while waiting for VM {}'.format(vm_name))
-            vm_stop(vm_name)
-            break
-
-
-# Copy file from VM
-def vm_copyfrom(vm_name, vm_login, vm_guest_password, local_file, remote_file):
-    logging.info('Downloading file {} from VM {} as {}'.format(remote_file, vm_name, local_file))
-    a, b, c = vboxmanage(
-        ['guestcontrol', vm_name, '--username', vm_guest_username, '--password', vm_guest_password, 'copyfrom',
-         remote_file,
-         local_file])
-    if a == 0:
-        logging.info('File downloaded')
-    else:
-        logging.error('Error while downloading file. Code: {}'.format(a))
+    if _ >= timeout:
+        logging.error(f'{vm_name}({snapshot_name}): Timeout while executing file on VM')
+        exit()
 
 
 # Copy file to VM
-def vm_copyto(vm_name, vm_login, vm_guest_password, local_file, remote_file):
+def vm_copyto(vm_name, snapshot_name, vm_guest_username, vm_guest_password, local_file, remote_file):
     _ = 0
-    while _ < vm_start_timeout:
-        logging.info('Uploading file {} as {} to VM {}'.format(local_file, remote_file, vm_name))
-        a, b, c = vboxmanage(
-            ['guestcontrol', vm_name, '--username', vm_guest_username, '--password', vm_guest_password, 'copyto',
-             local_file, remote_file])
-        if a == 0:
-            logging.info('File uploaded')
+    while _ < timeout:
+        logging.info(f'{vm_name}({snapshot_name}): Uploading file {local_file} as {remote_file} to VM')
+        cmd = subprocess.run([vboxmanage_path,
+                              'guestcontrol', vm_name, '--username', vm_guest_username, '--password',
+                              vm_guest_password, 'copyto',
+                              local_file, remote_file], capture_output=True, timeout=timeout, text=True)
+        if cmd.returncode == 0:
+            logging.debug(f'{vm_name}({snapshot_name}): File uploaded')
             break
         else:
-            logging.error('Error while uploading file. Code: {}'.format(a))
-        time.sleep(1)
-        _ += 1
-        if _ == vm_start_timeout:
-            logging.error('Timeout while waiting for VM {}'.format(vm_name))
-            exit()
+            logging.error(f'{vm_name}({snapshot_name}): Error while uploading file. Code: {cmd.returncode}')
+    time.sleep(1)
+    _ += 1
+    if _ >= timeout:
+        logging.error(f'{vm_name}({snapshot_name}): Timeout while waiting for VM')
+        exit()
+
+
+# Copy file from VM
+def vm_copyfrom(vm_name, snapshot_name, vm_guest_username, vm_guest_password, local_file, remote_file):
+    logging.info(f'{vm_name}({snapshot_name}): Downloading file {remote_file} from VM as {local_file}')
+    cmd = subprocess.run([vboxmanage_path,
+                          'guestcontrol', vm_name, '--username', vm_guest_username, '--password',
+                          vm_guest_password, 'copyfrom',
+                          remote_file, local_file], capture_output=True, timeout=timeout, text=True)
+    if cmd.returncode == 0:
+        logging.debug(f'{vm_name}({snapshot_name}): File downloaded')
+    else:
+        logging.error(f'{vm_name}({snapshot_name}): Error while downloading file. Code: {cmd.returncode}')
 
 
 # Take screenshot
-def vm_screenshot(vm_name, screenshot_name):
-    logging.info('Taking screenshot on VM {} as {}'.format(vm_name, screenshot_name))
-    vboxmanage(['controlvm', vm_name, 'screenshotpng', screenshot_name])
+def vm_screenshot(vm_name, snapshot_name, image_id=1):
+    screenshot_name = f'{vm_name}_{snapshot_name}_{image_id}.png'
+    logging.info(f'{vm_name}({snapshot_name}): Taking screenshot {screenshot_name}')
+    subprocess.run([vboxmanage_path, 'controlvm', vm_name, 'screenshotpng', screenshot_name], capture_output=True,
+                   timeout=timeout, text=True)
+    image_id += 1
+    return image_id
 
 
 # Main routines
-logging.info('VMs: {}; Snapshots: {}'.format(str(vms_list), str(snapshots_list)))
-vm_version()
-for vm_name in vms_list:
+def main_routine(vm_name, snapshots_list):
     for snapshot_name in snapshots_list:
-        logging.info('Using VM {} with snapshot {}'.format(vm_name, snapshot_name))
-        vm_stop(vm_name)
+        logging.info(f'{vm_name}({snapshot_name}): Task started')
+        vm_stop(vm_name, snapshot_name)
         vm_restore(vm_name, snapshot_name)
-        vm_start(vm_name)
-        remote_file = remote_folder + str(random.randrange(1000000000)) + ''.join(local_file_extension)
-        vm_copyto(vm_name, vm_guest_username, vm_guest_password, local_file, remote_file)
-        vm_screenshot(vm_name, '{}_{}_1.png'.format(vm_name, snapshot_name))
-        vm_start_on_vm(vm_name, vm_guest_username, vm_guest_password, remote_file)
+
+        vm_start(vm_name, snapshot_name)
+
+        if 'local_file_extension' not in locals():
+            # Assuming '.exe' if no extension extracted from name
+            local_file_extension = '.exe'
+        random_name = ''.join(random.choice(string.ascii_letters) for x in range(random.randint(4, 20)))
+        remote_file = remote_folder + random_name + ''.join(local_file_extension)
+        vm_copyto(vm_name, snapshot_name, vm_guest_username, vm_guest_password, local_file, remote_file)
+        screenshot = vm_screenshot(vm_name, snapshot_name)
+        vm_start_on_vm(vm_name, snapshot_name, vm_guest_username, vm_guest_password, remote_file)
         time.sleep(5)
-        vm_screenshot(vm_name, '{}_{}_2.png'.format(vm_name, snapshot_name))
-        logging.info('Waiting for results. Timeout: {} seconds.'.format(vm_delay))
-        time.sleep(vm_delay)
-        vm_screenshot(vm_name, '{}_{}_3.png'.format(vm_name, snapshot_name))
-        vm_stop(vm_name)
+        screenshot = vm_screenshot(vm_name, snapshot_name, screenshot)
+        time.sleep(timeout)
+        screenshot = vm_screenshot(vm_name, snapshot_name, screenshot)
+
+        vm_stop(vm_name, snapshot_name)
         vm_restore(vm_name, snapshot_name)
-        logging.info('Done')
+        logging.info(f'{vm_name}({snapshot_name}): Task finished')
+
+
+max_threads = 2
+# Show general info
+logging.info(f'Using VMs: {vms_list} with snapshots: {snapshots_list}')
+logging.info(f'Max threads: {threads}')
+logging.info(f'Using file: "{local_file}"\n')
+time.sleep(1)
+
+# Start threads
+for vm_name in vms_list:
+    t = threading.Thread(target=main_routine, args=(vm_name, snapshots_list))
+    t.start()
+    time.sleep(3)
